2a23133df69cdebb2ce0648650d92602793d3db4
Joseph Lumbley <joelumbley16@gmail.com>
2023-08-06 22:34:02 -0500

    Add project files.

---
+++ Animation/Form1.vb
@@ -0,0 +1,301 @@
+﻿'Animation
+'
+'Animation is the art of creating the illusion of motion by displaying a series of static images in quick succession.
+'In our app, we use animation to make it appear as though our rectangle is moving towards the right.
+'To ensure that our animation runs smoothly on all devices, we have designed it to be frame independent.
+'This means that our animation is not affected by changes in the frame rate,
+'ensuring a consistent and seamless experience for all users.
+
+'MIT License
+'Copyright(c) 2023 Joseph W. Lumbley
+
+'Permission Is hereby granted, free Of charge, to any person obtaining a copy
+'of this software And associated documentation files (the "Software"), to deal
+'in the Software without restriction, including without limitation the rights
+'to use, copy, modify, merge, publish, distribute, sublicense, And/Or sell
+'copies of the Software, And to permit persons to whom the Software Is
+'furnished to do so, subject to the following conditions:
+
+'The above copyright notice And this permission notice shall be included In all
+'copies Or substantial portions of the Software.
+
+'THE SOFTWARE Is PROVIDED "AS IS", WITHOUT WARRANTY Of ANY KIND, EXPRESS Or
+'IMPLIED, INCLUDING BUT Not LIMITED To THE WARRANTIES Of MERCHANTABILITY,
+'FITNESS FOR A PARTICULAR PURPOSE And NONINFRINGEMENT. IN NO EVENT SHALL THE
+'AUTHORS Or COPYRIGHT HOLDERS BE LIABLE For ANY CLAIM, DAMAGES Or OTHER
+'LIABILITY, WHETHER In AN ACTION Of CONTRACT, TORT Or OTHERWISE, ARISING FROM,
+'OUT OF Or IN CONNECTION WITH THE SOFTWARE Or THE USE Or OTHER DEALINGS IN THE
+'SOFTWARE.
+
+'Monica is our an AI assistant.
+'https://monica.im/
+
+'I'm making a video to explain the code on my YouTube channel.
+'https://www.youtube.com/@codewithjoe6074
+'
+
+Imports System.Threading
+Imports System.Numerics
+Imports System.ComponentModel
+
+Public Class Form1
+
+    Private Context As New BufferedGraphicsContext
+
+    Private Buffer As BufferedGraphics
+
+    Private FrameCount As Integer = 0
+
+    Private StartTime As DateTime = Now 'Get current time.
+
+    Private TimeElapsed As TimeSpan
+
+    Private SecondsElapsed As Double = 0
+
+    Private FPS As Integer = 0
+
+    Private ReadOnly FPSFont As New Font(FontFamily.GenericSansSerif, 25)
+
+    Private FPS_Postion As New Point(0, 0)
+
+    Private Rect As New Rectangle(0, 100, 300, 300)
+
+    Private RectPostion As New Vector2(Rect.X, Rect.Y)
+
+    Private CurrentFrame As DateTime = Now 'Get current time.
+
+    Private LastFrame As DateTime = CurrentFrame 'Initialize last frame time to current time.
+
+    Private DeltaTime As TimeSpan = CurrentFrame - LastFrame 'Initialize delta time to 0
+
+    Private Velocity As Single = 250.0F
+
+    Private ReadOnly AlineCenter As New StringFormat With {.Alignment = StringAlignment.Center}
+
+    Private ReadOnly AlineCenterMiddle As New StringFormat With {.Alignment = StringAlignment.Center,
+                                                                 .LineAlignment = StringAlignment.Center}
+
+    Private GameLoopCancellationToken As New CancellationTokenSource()
+
+    Private ReadOnly CWJFont As New Font(FontFamily.GenericSansSerif, 38)
+
+    'For uncapped frame rate use GameLoopTask
+    Private GameLoopTask As Task =
+        Task.Factory.StartNew(Sub()
+                                  Try
+
+                                      Thread.CurrentThread.Priority = ThreadPriority.Normal
+
+                                      Do While Not GameLoopCancellationToken.IsCancellationRequested
+
+                                          UpdateFrame()
+
+                                          'Refresh the form to trigger a redraw.
+                                          If Not Me.IsDisposed AndAlso Me.IsHandleCreated Then
+
+                                              Me.Invoke(Sub() Me.Refresh())
+
+                                          End If
+
+                                          ' Wait for next frame
+                                          Thread.Sleep(TimeSpan.Zero)
+
+                                          'Thread.Sleep(TimeSpan.Zero), the thread relinquishes the
+                                          'remainder of its time slice to any thread of equal priority
+                                          'that is ready to run. If there are no other threads of equal
+                                          'priority that are ready to run, execution of the current thread is not suspended.
+
+                                      Loop
+
+                                      End
+
+                                  Catch ex As Exception
+
+                                      Debug.WriteLine(ex.ToString())
+
+                                  End Try
+
+                              End Sub)
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        InitializeApp()
+
+    End Sub
+
+    Private Sub InitializeApp()
+
+        InitializeForm()
+
+        InitializeBuffer()
+
+        InitializeTimer1()
+
+    End Sub
+
+    Private Sub InitializeForm()
+
+        Text = "Animation - Code with Joe"
+
+        SetStyle(ControlStyles.UserPaint, True)
+
+        SetStyle(ControlStyles.OptimizedDoubleBuffer, True)
+
+        SetStyle(ControlStyles.AllPaintingInWmPaint, True)
+
+    End Sub
+
+    Private Sub InitializeTimer1()
+
+        'For a capped frame rate use timer and set interval.
+        'Set tick rate to 60 ticks per second. 1 second = 1000 milliseconds.
+        Timer1.Interval = 15 '16.66666666666667 ms = 1000 ms / 60 ticks
+
+        'Timer1.Start()
+
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        'Every tick of timer do the following...
+
+        UpdateFrame()
+
+        Refresh() 'Calls OnPaint Event
+
+    End Sub
+
+    Private Sub UpdateFrame()
+
+        UpdateDeltaTime()
+
+        MoveRectangle()
+
+    End Sub
+
+    Private Sub MoveRectangle()
+
+        'Move the rectangle to the right.
+        RectPostion.X += Velocity * DeltaTime.TotalSeconds 'Δs = V * Δt
+        'Displacement = Velocity x Delta Time
+
+        'Wraparound
+        'When the rectangle exits the right side of the client area.
+        If RectPostion.X > ClientRectangle.Right Then
+
+            'The rectangle reappears on the left side the client area.
+            RectPostion.X = ClientRectangle.Left - Rect.Width
+
+        End If
+
+        Rect.X = Math.Round(RectPostion.X)
+
+    End Sub
+
+    Private Sub UpdateDeltaTime()
+        'Delta time (Δt) is the elapsed time since the last frame.
+
+        CurrentFrame = Now
+
+        DeltaTime = CurrentFrame - LastFrame 'Calculate delta time
+
+        LastFrame = Now 'Update last frame time
+
+    End Sub
+
+    Private Sub InitializeBuffer()
+
+        'Set context to the context of this app.
+        Context = BufferedGraphicsManager.Current
+
+        'Set buffer size to the primary working area.
+        Context.MaximumBuffer = Screen.PrimaryScreen.WorkingArea.Size
+
+        'Create buffer.
+        Buffer = Context.Allocate(CreateGraphics(), ClientRectangle)
+
+    End Sub
+
+    Protected Overrides Sub OnPaint(ByVal e As PaintEventArgs)
+
+        Buffer.Graphics.Clear(Color.Black)
+
+        Buffer.Graphics.FillRectangle(Brushes.Purple, Rect)
+
+        Buffer.Graphics.DrawString("Code with Joe", CWJFont, Brushes.White, Rect, AlineCenterMiddle)
+
+        'Draw frames per second display.
+        Buffer.Graphics.DrawString(FPS.ToString & " FPS", FPSFont, Brushes.MediumOrchid, FPS_Postion)
+
+        'Show buffer on form.
+        Buffer.Render(e.Graphics)
+
+        'Release memory used by buffer.
+        Buffer.Dispose()
+        Buffer = Nothing
+
+        'Create new buffer.
+        Buffer = Context.Allocate(CreateGraphics(), ClientRectangle)
+
+        'Use these settings when drawing to the backbuffer.
+        With Buffer.Graphics
+
+            'Bug Fix
+            .CompositingMode = Drawing2D.CompositingMode.SourceOver 'Don't Change.
+            'To fix draw string error with anti aliasing: "Parameters not valid."
+            'I set the compositing mode to source over.
+
+            .TextRenderingHint = Drawing.Text.TextRenderingHint.AntiAliasGridFit
+            .SmoothingMode = Drawing2D.SmoothingMode.AntiAlias
+            .CompositingQuality = Drawing2D.CompositingQuality.HighQuality
+            .InterpolationMode = Drawing2D.InterpolationMode.HighQualityBicubic
+            .PixelOffsetMode = Drawing2D.PixelOffsetMode.HighQuality
+
+        End With
+
+        UpdateFrameCounter()
+
+    End Sub
+
+    Private Sub UpdateFrameCounter()
+
+        TimeElapsed = Now.Subtract(StartTime)
+
+        SecondsElapsed = TimeElapsed.TotalSeconds
+
+        If SecondsElapsed < 1 Then
+
+            FrameCount += 1
+
+        Else
+
+            FPS = FrameCount
+
+            FrameCount = 0
+
+            StartTime = Now
+
+        End If
+
+    End Sub
+
+    Private Sub Form1_Resize(sender As Object, e As EventArgs) Handles MyBase.Resize
+
+        FPS_Postion.Y = ClientRectangle.Bottom - 75
+
+    End Sub
+
+    Private Sub Form1_Closing(sender As Object, e As CancelEventArgs) Handles MyBase.Closing
+
+        GameLoopCancellationToken.Cancel(True)
+
+    End Sub
+
+    Protected Overrides Sub OnPaintBackground(ByVal e As PaintEventArgs)
+
+        'Intentionally left blank. Do not remove.
+
+    End Sub
+
+End Class
+
+
